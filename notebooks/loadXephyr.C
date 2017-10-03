3a92493ca0e36e610e1b1b147988a1e3ebaf17b3
Ale <pan.manfredini@gmail.com>
2017-10-03 15:44:59 +0300

    adding notebooks

---
+++ notebooks/loadXephyr.C
@@ -0,0 +1,10 @@
+int loadXephyr(){
+  gROOT->ProcessLine(".L ../src/XeCore.cxx+g");
+  gROOT->ProcessLine(".L ../src/XeMath.cxx+g");
+  gROOT->ProcessLine(".L ../src/XeStat.cxx+g");
+  gROOT->ProcessLine(".L ../src/dataHandler.cxx+g");
+  gROOT->ProcessLine(".L ../src/XePdfObjects.cxx+g");
+  gROOT->ProcessLine(".L ../src/XeLikelihoods.cxx+g");
+  gROOT->ProcessLine(".L ../src/AsymptoticExclusion.cxx+g");
+  return 0;
+}
