f175734865f5818d63061078ce64ae42eeaeeba3
Ale <pan.manfredini@gmail.com>
2018-06-04 12:00:37 +0300

    load xephyr

--- loadXephyr.C
+++ loadXephyr.C
@@ -1,15 +1,16 @@
 int loadXephyr(){
+ 
+  TString xeDir(gSystem->Getenv("XEPHYR_DIR"));
 
-  gROOT->ProcessLine(".L src/XeVersion.cxx+g");
-  gROOT->ProcessLine(".L src/XeUtils.cxx+g");
-  gROOT->ProcessLine(".L src/XeStat.cxx+g");
-  gROOT->ProcessLine(".L src/dataHandler.cxx+g");
-  gROOT->ProcessLine(".L src/XePdfObjects.cxx+g");
-  gROOT->ProcessLine(".L src/XeLikelihoods.cxx+g");
-  gROOT->ProcessLine(".L src/AsymptoticExclusion.cxx+g");
-  gROOT->ProcessLine(".L src/ToyGenerator.cxx+g");
-  gROOT->ProcessLine(".L src/plotHelpers.cxx+g");
-  gROOT->ProcessLine(".L  src/ToyFitterExclusion.cxx+g");
-
+  gROOT->ProcessLine(".L " + xeDir +"/Xephyr/src/XeVersion.cxx+g");
+  gROOT->ProcessLine(".L  " + xeDir +"/Xephyr/src/XeUtils.cxx+g");
+  gROOT->ProcessLine(".L  " + xeDir +"/Xephyr/src/XeStat.cxx+g");
+  gROOT->ProcessLine(".L  " + xeDir +"/Xephyr/src/dataHandler.cxx+g");
+  gROOT->ProcessLine(".L  " + xeDir +"/Xephyr/src/XePdfObjects.cxx+g");
+  gROOT->ProcessLine(".L  " + xeDir +"/Xephyr/src/XeLikelihoods.cxx+g");
+  gROOT->ProcessLine(".L  " + xeDir +"/Xephyr/src/AsymptoticExclusion.cxx+g");
+  gROOT->ProcessLine(".L  " + xeDir +"/Xephyr/src/ToyGenerator.cxx+g");
+  gROOT->ProcessLine(".L  " + xeDir +"/Xephyr/src/plotHelpers.cxx+g");
+  gROOT->ProcessLine(".L  " + xeDir +"/Xephyr/src/ToyFitterExclusion.cxx+g");
   return 0;
 }
