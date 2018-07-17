15d581876e7977ee44ad17629ee45274e0b798fa
panManfredini <pan.manfredini@gmail.com>
2018-07-17 13:23:12 +0200

    some more comment on asymptotics limit

--- examples/SR1Like/asymptotic_limit.C
+++ examples/SR1Like/asymptotic_limit.C
@@ -4,8 +4,18 @@
 
 	// loading the likelihood definition
 	 gROOT->ProcessLine(".L src/likelihoodDef.cxx");
+        
+	
+	// this is to load a combined likelihood of 3 volumes, "other" is default to zero
+	// and is not needed for SR1, but could be usefull as additional parameter for your signal model
+	// See signalDef.h  	
+	// CombinedProfileLikelihood* getDMCombinedLikelihood( double mass , double other = 0.) {
 
-	 pdfLikelihood *pl = getDMLikelihood(50., 0);
+	 CombinedProfileLikelihood *pl = getDMCombinedLikelihood(50. );
+	
+	 // this is to load a likelihood only for one volume [0=egg, 1=U-volume, 2=wall]
+	 // other as above is dummy. 
+	 // pdfLikelihood* getDMLikelihood(double mass , int volume, double other=0 );
 
 
 
